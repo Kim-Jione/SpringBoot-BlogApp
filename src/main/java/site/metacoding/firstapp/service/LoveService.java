035793a2fa22c2d3aea56e566f7a224ac77d90b1
kim_jiwon <106166409+Kim-Jione@users.noreply.github.com>
2022-12-06 17:47:02 +0900

    좋아요 코드 리펙토링 (#5)

--- src/main/java/site/metacoding/firstapp/service/LoveService.java
+++ src/main/java/site/metacoding/firstapp/service/LoveService.java
@@ -1,13 +1,10 @@
 package site.metacoding.firstapp.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.firstapp.domain.love.Love;
 import site.metacoding.firstapp.domain.love.LoveDao;
-import site.metacoding.firstapp.web.dto.response.love.DeleteRespDto;
-import site.metacoding.firstapp.web.dto.response.love.LoveRespDto;
 
 @RequiredArgsConstructor
 @Service
@@ -15,19 +12,17 @@ public class LoveService {
 
 	private final LoveDao loveDao;
 
-	@Transactional
-	public LoveRespDto 좋아요(Integer userId, Integer postId) {
-		Love love = new Love(userId, postId);
-		loveDao.insert(love);
-		LoveRespDto loveRespDto = loveDao.loveResult(userId, postId);
-		return loveRespDto;
+	public Integer 좋아요Id불러오기(Integer userId, Integer postId) {
+		return loveDao.findByUserIdAndPostId(userId, postId);
 	}
 
-	@Transactional
-	public DeleteRespDto 좋아요취소(Integer loveId) {
-		DeleteRespDto deleteRespDto = loveDao.deleteResult(loveId);
+	public void 좋아요(Integer userId, Integer postId) {
+		Love love = new Love(userId, postId);
+		loveDao.insert(love);
+	}
+
+	public void 좋아요취소(Integer loveId) {
 		loveDao.delete(loveId);
-		return deleteRespDto;
 	}
 
 }
--- src/main/java/site/metacoding/firstapp/web/LoveController.java
+++ src/main/java/site/metacoding/firstapp/web/LoveController.java
@@ -2,7 +2,6 @@ package site.metacoding.firstapp.web;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.firstapp.service.LoveService;
 import site.metacoding.firstapp.web.dto.CMRespDto;
-import site.metacoding.firstapp.web.dto.response.love.DeleteRespDto;
 import site.metacoding.firstapp.web.dto.response.love.LoveRespDto;
 import site.metacoding.firstapp.web.dto.response.user.SessionUserDto;
 
@@ -22,27 +20,26 @@ public class LoveController {
 	private final LoveService loveService;
 
 	// 게시글 좋아요 응답
-	@PostMapping("/love/{postId}")
-	public @ResponseBody CMRespDto<?> insertLove(@PathVariable Integer postId) {
+	@PostMapping("/love/{postId}") // 좋아요한 게시글
+	public @ResponseBody CMRespDto<?> love(@PathVariable Integer postId) {
 		SessionUserDto principal = (SessionUserDto) session.getAttribute("principal");
+
 		if (principal == null) {
 			return new CMRespDto<>(-1, "로그인을 진행해주세요.", null);
 		}
-		LoveRespDto loveRespDto = loveService.좋아요(principal.getUserId(), postId);
+		Integer loveId = loveService.좋아요Id불러오기(principal.getUserId(), postId);
 
-		return new CMRespDto<>(1, "좋아요 성공", loveRespDto);
-	}
-	
-	// 게시글 싫어요 응답
-	@DeleteMapping("/love/delete/{loveId}")
-	public @ResponseBody CMRespDto<?> deleteLove(@PathVariable Integer loveId) {
-		SessionUserDto principal = (SessionUserDto) session.getAttribute("principal");
-		if (principal == null) {
-			return new CMRespDto<>(-1, "로그인을 진행해주세요.", null);
+		if (loveId == null) {
+			loveService.좋아요(principal.getUserId(), postId);
+			loveId = loveService.좋아요Id불러오기(principal.getUserId(), postId);
+			LoveRespDto loveRespDto = new LoveRespDto(loveId, principal.getUserId(), postId);
+			return new CMRespDto<>(1, "좋아요 성공", loveRespDto);
 		}
-		DeleteRespDto deleteRespDto = loveService.좋아요취소(loveId);
 
-		return new CMRespDto<>(1, "좋아요 취소 성공", deleteRespDto);
+		LoveRespDto loveRespDto = new LoveRespDto(loveId, principal.getUserId(), postId);
+		loveService.좋아요취소(loveId);
+
+		return new CMRespDto<>(1, "좋아요 취소 성공", loveRespDto);
 	}
 
 }
